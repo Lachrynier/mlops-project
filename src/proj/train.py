02b6439ae7f430997b6320a7b067a70cfaf834b6
Bastian Ravn <Bastian>
2025-01-17 10:43:28 +0100

    updated training dockerfile and added model unit test

--- src/proj/train.py
+++ src/proj/train.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 import os
 import wandb
 
-from model import create_model
+from proj.model import create_model
 
 
 def train(lr: float=1e-3):
@@ -22,7 +22,7 @@ def train(lr: float=1e-3):
 
     device = ('cuda' if torch.cuda.is_available() else 'cpu')
 
-    train_dataset = torch.load("data/processed/subset10_train.pt")
+    train_dataset = torch.load("data/processed/subset10_train.pt", weights_only=True)
     train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size)
 
     model = create_model(num_classes=10).to(device)
