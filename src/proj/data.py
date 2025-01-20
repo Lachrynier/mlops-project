183bda34e4923a5cfd98155f0848c700eda5f11d
David <97088473+Lachrynier@users.noreply.github.com>
2025-01-20 18:53:25 +0100

    Revert "Refactor data and solve mkdocs dependency conflict (#22)"
    This reverts commit a07103452b5de4654e077e69cb10c61e107fc498.

--- src/proj/data.py
+++ src/proj/data.py
@@ -109,7 +109,9 @@ def preprocess_subset(
         [i for i, target in enumerate(dataset.targets) if target < num_classes]
     )
 
-    train_subset, test_subset = random_split(subset, [1 - test_ratio, test_ratio])
+    test_size = int(len(subset) * test_ratio)
+    train_size = len(subset) - test_size
+    train_subset, test_subset = random_split(subset, [train_size, test_size])
 
     print("Constructing training set...")
     train_subset = tuple(zip(*train_subset, strict=True))
