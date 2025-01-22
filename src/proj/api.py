75943f434622eeaf1a0225b29bd81906e9f2740c
David Johansen <diamand885@gmail.com>
2025-01-22 03:33:26 +0100

    deployed app to gcp

--- src/proj/api.py
+++ src/proj/api.py
@@ -1,4 +1,3 @@
-import uvicorn
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
 from io import BytesIO
@@ -8,11 +7,19 @@ import torch
 from proj.model import create_model
 from proj.data import TRANSFORM
 
+from google.cloud import storage
+import os
+
+storage_client = storage.Client()
+bucket_name = os.environ['BUCKET_NAME']
+bucket = storage_client.bucket(bucket_name)
+blob = bucket.blob('models/model.pth')
+
+model_path = 'model.pth'
+blob.download_to_filename(model_path)
+
+
 DEVICE = ('cuda' if torch.cuda.is_available() else 'cpu')
-
-app = FastAPI()
-
-model_path = "./models/model.pth"
 
 # If the whole model is saved:
 # model = torch.load(model_path)
@@ -23,6 +30,9 @@ model = create_model(num_classes=10).to(DEVICE)
 model.load_state_dict(torch.load(model_path))
 model.eval()
 
+
+app = FastAPI()
+
 @app.post("/predict/")
 async def predict(image: UploadFile = File(...)):
     try:
@@ -32,7 +42,7 @@ async def predict(image: UploadFile = File(...)):
         image_bytes = await image.read()
         img = Image.open(BytesIO(image_bytes))
 
-        input_tensor = TRANSFORM(img).unsqueeze(0)
+        input_tensor = TRANSFORM(img).unsqueeze(0).to(DEVICE)
 
         with torch.no_grad():
             output = model(input_tensor)
@@ -45,5 +55,6 @@ async def predict(image: UploadFile = File(...)):
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
     
 # Local debugging
+# import uvicorn
 # if __name__ == "__main__":
 #     uvicorn.run(app, host="0.0.0.0", port=8000)
